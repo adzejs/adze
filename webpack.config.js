cac0b60f2435984faf4006f0da6a9825d7dd175f
Andrew Stacy <astacy@stratacache.com>
2020-01-22 22:28:11 -0500

    Removed the babel lodash plugin as its unneeded.

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,6 @@ module.exports = env => ({
               "@babel/plugin-proposal-class-properties",
               "@babel/plugin-proposal-optional-chaining",
               "@babel/plugin-proposal-nullish-coalescing-operator",
-              "lodash"
             ]
           }
         }
