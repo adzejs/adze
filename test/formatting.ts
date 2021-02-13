b40da38802a5e8ba2416d7e29c15e44a4b4952d7
Andrew Stacy <andrew@andrews.mbp.lan>
2021-02-12 21:13:41 -0500

    Added unit tests for dirxml and table modifiers.

--- test/formatting.ts
+++ test/formatting.ts
@@ -1,20 +1,45 @@
 import test from 'ava';
-import { adze, createShed, removeShed } from '../src';
+import { adze } from '../src';
 
 global.ADZE_ENV = 'dev';
 
 test('dir log terminates properly', (t) => {
-  const { log, render } = adze().dir.info({ a: 1234, b: 5678 });
+  const { render } = adze().dir.info({ a: 1234, b: 5678 });
   if (render) {
     const [method, args] = render;
     t.is(method, 'dir');
     t.deepEqual(args[0], { a: 1234, b: 5678 });
-    t.pass();
   } else {
     t.fail();
   }
 });
 
-test.todo('dirxml log terminates properly');
+test('dirxml log terminates properly', (t) => {
+  const { render } = adze().dirxml.info(
+    '<a href="http://www.andrewstacy.com/></a>'
+  );
+  if (render) {
+    const [method, args] = render;
+    t.is(method, 'dirxml');
+    t.is(args[0], '<a href="http://www.andrewstacy.com/></a>');
+  } else {
+    t.fail();
+  }
+});
 
-test.todo('table log terminates properly.');
+test('table log terminates properly.', (t) => {
+  const { render } = adze().table.info([
+    { firstName: 'Andrew', lastName: 'Stacy' },
+    { firstName: 'Jim', lastName: 'Bob' },
+  ]);
+  if (render) {
+    const [method, args] = render;
+    t.is(method, 'table');
+    t.deepEqual(args[0], [
+      { firstName: 'Andrew', lastName: 'Stacy' },
+      { firstName: 'Jim', lastName: 'Bob' },
+    ]);
+  } else {
+    t.fail();
+  }
+});
