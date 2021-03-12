3c8ef546160e1b9a232e2bf08ea352449c1e0bc9
Andrew Stacy <andrew@andrews.mbp.lan>
2021-03-11 21:55:41 -0500

    Created unit tests for identifying modifiers on node logs. Added a unit test for multiple namespaces applied to browser logs.

--- test/browser/modifiers/identifying.ts
+++ test/browser/modifiers/identifying.ts
@@ -9,7 +9,7 @@ browserEnv();
 window.ADZE_ENV = 'dev';
 
 test('label prints correctly', (t) => {
-  const { log, render } = adze().label('test').log('This log has a label.');
+  const { render } = adze().label('test').log('This log has a label.');
   if (render) {
     const [_, args] = render;
     t.is(args[2], '[test] ');
@@ -19,10 +19,20 @@ test('label prints correctly', (t) => {
 });
 
 test('log with single namespace prints correctly', (t) => {
-  const { log, render } = adze().ns('test').log('This log has a label.');
+  const { render } = adze().ns('test').log('This log has a label.');
   if (render) {
     const [_, args] = render;
     t.is(args[2], '#test ');
+  } else {
+    t.fail();
+  }
+});
+
+test('log with multiple namespaces prints correctly', (t) => {
+  const { render } = adze().ns(['test', 'test2']).log('This log has a label.');
+  if (render) {
+    const [_, args] = render;
+    t.is(args[2], '#test #test2 ');
   } else {
     t.fail();
   }
--- test/node/modifiers/identifying.ts
+++ test/node/modifiers/identifying.ts
@@ -1,28 +1,33 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const browserEnv = require('browser-env');
 import test from 'ava';
 import { adze } from '../../../src';
 
-// Simulate the browser environment for testing
-browserEnv();
-// Our global context is the window not global
-window.ADZE_ENV = 'dev';
+global.ADZE_ENV = 'dev';
 
 test('label prints correctly', (t) => {
-  const { log, render } = adze().label('test').log('This log has a label.');
+  const { render } = adze().label('test').log('This log has a label.');
   if (render) {
     const [_, args] = render;
-    t.is(args[2], '[test] ');
+    t.is(args[1], '[test] ');
   } else {
     t.fail();
   }
 });
 
 test('log with single namespace prints correctly', (t) => {
-  const { log, render } = adze().ns('test').log('This log has a label.');
+  const { render } = adze().ns('test').log('This log has a label.');
   if (render) {
     const [_, args] = render;
-    t.is(args[2], '#test ');
+    t.is(args[1], '#test ');
+  } else {
+    t.fail();
+  }
+});
+
+test('log with multiple namespaces prints correctly', (t) => {
+  const { render } = adze().ns(['test', 'test2']).log('This log has a label.');
+  if (render) {
+    const [_, args] = render;
+    t.is(args[1], '#test #test2 ');
   } else {
     t.fail();
   }
