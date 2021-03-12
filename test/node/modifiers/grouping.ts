a931b96052e1170a08aad714cfd8a1dbcabe28b5
Andrew Stacy <andrew@andrews.mbp.lan>
2021-03-11 21:51:32 -0500

    Created unit tests for grouping modifiers on node logs.

--- test/node/modifiers/grouping.ts
+++ test/node/modifiers/grouping.ts
@@ -1,12 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const browserEnv = require('browser-env');
 import test from 'ava';
-import { adze, defaults } from '../../../src';
+import { adze } from '../../../src';
 
-// Simulate the browser environment for testing
-browserEnv();
-// Our global context is the window not global
-window.ADZE_ENV = 'dev';
+global.ADZE_ENV = 'dev';
 
 test('group log renders correctly', (t) => {
   const { log, render } = adze().group.success('Opening a log group.');
@@ -14,9 +9,8 @@ test('group log renders correctly', (t) => {
     const [method, args] = render;
     t.is(method, 'group');
     t.is(log.data.level, 5);
-    t.is(args[0], ' %c Success(1)');
-    t.is(args[1], `${defaults.base_style}${defaults.log_levels.success.style}`);
-    t.is(args[2], 'Opening a log group.');
+    t.is(args[0], ' Success(1)    ');
+    t.is(args[1], 'Opening a log group.');
   } else {
     t.fail();
   }
@@ -30,9 +24,8 @@ test('group collapsed renders correctly', (t) => {
     const [method, args] = render;
     t.is(method, 'groupCollapsed');
     t.is(log.data.level, 5);
-    t.is(args[0], ' %c Success(1)');
-    t.is(args[1], `${defaults.base_style}${defaults.log_levels.success.style}`);
-    t.is(args[2], 'Opening a collapsed log group.');
+    t.is(args[0], ' Success(1)    ');
+    t.is(args[1], 'Opening a collapsed log group.');
   } else {
     t.fail();
   }
