5e216d193743497fe5020bbae811a30d0ef9b788
Andrew Stacy <ajstacy@gmail.com>
2022-03-16 13:45:26 -0400

    fix(timestamp): timestamp incorrectly formatted
    Fixes bug where timestamp was incorrectly printing UTC time rather than local time.

--- test/browser/modifiers/timing.ts
+++ test/browser/modifiers/timing.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const browserEnv = require('browser-env');
 import test from 'ava';
-import { formatISO } from 'date-fns';
+import { format } from 'date-fns';
 import adze, { createShed, removeShed } from '../../../src';
 
 // Simulate the browser environment for testing
@@ -52,12 +52,15 @@ test('renders iso8601 timestamp properly', (t) => {
   if (args && milli && typeof args[2] === 'string') {
     // Get a date object to generate a timestamp with date-fns to check for accuracy
     const compareDate = new Date(milli);
+    const ymd = format(compareDate, 'yyyy-MM-dd');
+    const hmsSx = format(compareDate, 'HH:mm:ss.SSSXXX');
+    const compareDateStr = `${ymd}T${hmsSx}  `;
 
     t.regex(
       args[2],
-      /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])([+-][0-9]{2}:[0-9]+)?(Z)?\s\s$/g
+      /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9]).[0-9]{3}([+-][0-9]{2}:[0-9]+)?(Z)?\s+$/g
     );
-    t.is(args[2], `${formatISO(compareDate)}  `);
+    t.is(args[2], compareDateStr);
   } else {
     t.fail();
   }
--- test/node/modifiers/timing.ts
+++ test/node/modifiers/timing.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { formatISO } from 'date-fns';
+import { format } from 'date-fns';
 import adze, { createShed, removeShed } from '../../../src';
 
 global.ADZE_ENV = 'dev';
@@ -47,12 +47,16 @@ test('renders iso8601 timestamp properly', (t) => {
   if (args && milli && typeof args[1] === 'string') {
     // Get a date object to generate a timestamp with date-fns to check for accuracy
     const compareDate = new Date(milli);
+    const ymd = format(compareDate, 'yyyy-MM-dd');
+    const hmsSx = format(compareDate, 'HH:mm:ss.SSSXXX');
+    const compareDateStr = `${ymd}T${hmsSx}  `;
 
     t.regex(
       args[1],
-      /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])([+-][0-9]{2}:[0-9]+)?(Z)?\s\s$/g
+      /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9]).[0-9]{3}([+-][0-9]{2}:[0-9]+)?(Z)?\s+$/g
     );
-    t.is(args[1], `${formatISO(compareDate)}  `);
+
+    t.is(args[1], compareDateStr);
   } else {
     t.fail();
   }
