6993d3d32b8db0b872eae199027283713fb09fa6
Andrew Stacy <astacy@stratacache.com>
2024-09-09 17:57:32 -0400

    ci: attempting publish to v1 directory

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,8 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: description,
-
+  // Base url
+  base: '/v1/',
   /**
    * Extra tags to be injected to the page HTML `<head>`
    *
