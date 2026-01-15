8ec3d8a15e450ec8198fd888fe45613a6827b469
Andrew Stacy <astacy@stratacache.com>
2026-01-15 13:32:16 -0500

    ci: removing auto increment of package.json version by semantic release

--- release.config.js
+++ release.config.js
@@ -15,7 +15,6 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
@@ -24,12 +23,8 @@ export default {
       },
     ],
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    // Note: @semantic-release/git and @semantic-release/changelog removed because
+    // master branch protection prevents direct pushes. The changelog is included
+    // in the GitHub release notes instead.
   ],
 };
